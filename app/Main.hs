e1f8a12567e605bc26b44dbc7dedf584e73d4175
wasabi315 <wasabi.nvim.st@gmail.com>
2026-08-29 19:16:59 +0200

    Re-parallelize

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,10 @@ module Main (main, compSquareH, compSquareV) where
 
 import Common
 import Control.Exception
+import Control.Monad
 import ConversionModuloIsomorphism
 import Data.ByteString qualified as BS
-import Data.Foldable
 import Data.Time.Clock
-import DiscriminationTree
 import Evaluation
 import Heuristic
 import Isomorphism
@@ -25,54 +24,32 @@ main =
     ["bench1"] -> do
       Right ts <- unflat @[Term] <$> BS.readFile "bench.bin"
       ts <- evaluate $ force ts
-      (_, t) <- timed $ for_ ts \t -> case convIso0 compSquareH t of
-        _ : _ -> pure ()
-        [] -> error "bug in convIso0"
+      (_, t) <- timed do
+        let hits = map (not . null . convIso0 compSquareH) ts `using` parListChunk 128 rseq
+        unless (and hits) $ error "bug in convIso0"
       print t
     ["bench2"] -> do
       Right ts <- unflat @[Term] <$> BS.readFile "bench.bin"
       ts <- evaluate $ force ts
-      (_, t) <- timed $ for_ ts \t -> case convIso0 compSquareV t of
-        [] -> pure ()
-        _ : _ -> error "bug in convIso0"
+      (_, t) <- timed do
+        let hits = map (not . null . convIso0 compSquareV) ts `using` parListChunk 128 rseq
+        when (or hits) $ error "bug in convIso0"
       print t
     ["bench3"] -> do
       Right ts <- unflat @[Term] <$> BS.readFile "bench.bin"
       ts <- evaluate $ force ts
       (_, t) <- timed do
-        let dt = isoTrie 0 (eval emptyEnv compSquareH)
-        for_ ts \t -> do
-          case findConvIso 0 (eval emptyEnv t) dt of
-            _ : _ -> pure ()
-            [] -> error "bug in isoTrie/findConv"
+        let match = refineConvIso0 compSquareH
+            hits = map (not . null . match) ts `using` parListChunk 128 rseq
+        unless (and hits) $ error "bug in refineConvIso0"
       print t
     ["bench4"] -> do
       Right ts <- unflat @[Term] <$> BS.readFile "bench.bin"
       ts <- evaluate $ force ts
       (_, t) <- timed do
-        let dt = isoTrie 0 (eval emptyEnv compSquareV)
-        for_ ts \t -> do
-          case findConvIso 0 (eval emptyEnv t) dt of
-            [] -> pure ()
-            _ : _ -> error "bug in isoTrie/findConv"
-      print t
-    ["bench5"] -> do
-      Right ts <- unflat @[Term] <$> BS.readFile "bench.bin"
-      ts <- evaluate $ force ts
-      (_, t) <- timed do
-        let match = refineConvIso0 compSquareH
-        for_ ts \t -> case match t of
-          _ : _ -> pure ()
-          [] -> error "bug in refineConvIso0"
-      print t
-    ["bench6"] -> do
-      Right ts <- unflat @[Term] <$> BS.readFile "bench.bin"
-      ts <- evaluate $ force ts
-      (_, t) <- timed do
         let match = refineConvIso0 compSquareV
-        for_ ts \t -> case match t of
-          [] -> pure ()
-          _ : _ -> error "bug in refineConvIso0"
+            hits = map (not . null . match) ts `using` parListChunk 128 rseq
+        when (or hits) $ error "bug in refineConvIso0"
       print t
     _ -> error "invalid argument"
 
