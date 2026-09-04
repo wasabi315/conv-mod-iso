01c01995b16ccda854cd2f31e7144e6086e8090a
wasabi315 <wasabi.nvim.st@gmail.com>
2026-09-04 20:29:33 +0200

    Remove DiscrimTree

--- src/Common.hs
+++ src/Common.hs
@@ -17,7 +17,6 @@ import Control.Parallel.Strategies
 import Data.Coerce
 import Data.Generics.Labels ()
 import Data.Ix
-import Data.Monoid
 import Data.Primitive.SmallArray
 import Flat
 import GHC.Exts
@@ -47,71 +46,6 @@ updateSmallArray i ~x xs = runSmallArray do
   writeSmallArray ys i x
   pure ys
 {-# INLINE updateSmallArray #-}
-
-lookupSmallArray :: (Ord k) => k -> SmallArray (k, a) -> Maybe a
-lookupSmallArray k xs = go 0
-  where
-    n = sizeofSmallArray xs
-    go i
-      | i == n = Nothing
-      | (k', x) <- indexSmallArray xs i =
-          case compare k k' of
-            LT -> Nothing
-            EQ -> Just x
-            GT -> go (i + 1)
-{-# INLINE lookupSmallArray #-}
-
-insertSmallArray :: (Ord k, Semigroup a) => k -> a -> SmallArray (k, a) -> SmallArray (k, a)
-insertSmallArray tok ~x xs = runSmallArray do
-  let sz = sizeofSmallArray xs
-      go i
-        | i == sz = do
-            ys <- newSmallArray (sz + 1) (tok, x)
-            copySmallArray ys 0 xs 0 i
-            pure ys
-        | (tok', y) <- indexSmallArray xs i =
-            case compare tok tok' of
-              LT -> do
-                ys <- newSmallArray (sz + 1) (tok, x)
-                copySmallArray ys 0 xs 0 i
-                copySmallArray ys (i + 1) xs i (sz - i)
-                pure ys
-              EQ -> do
-                xs <- thawSmallArray xs 0 sz
-                writeSmallArray xs i (tok, x <> y)
-                pure xs
-              GT -> go (i + 1)
-
-  go 0
-{-# INLINEABLE insertSmallArray #-}
-
-insertSmallArrayR :: forall k a. (Ord k, Semigroup a) => k -> a -> SmallArray (k, a) -> SmallArray (k, a)
-insertSmallArrayR = coerce (insertSmallArray @k @(Dual a))
-{-# INLINE insertSmallArrayR #-}
-
-mergeSmallArray :: (Ord k, Semigroup a) => SmallArray (k, a) -> SmallArray (k, a) -> SmallArray (k, a)
-mergeSmallArray xs ys = runSmallArray do
-  let sz = sizeofSmallArray xs
-      sz' = sizeofSmallArray ys
-  zs <- newSmallArray (sz + sz') undefined
-
-  let go i j k
-        | i == sz = do
-            copySmallArray zs k ys j (sz' - j)
-            shrinkSmallMutableArray zs (k + sz' - j)
-        | j == sz' = do
-            copySmallArray zs k xs i (sz - i)
-            shrinkSmallMutableArray zs (k + sz - i)
-        | (# p@(tok, t) #) <- indexSmallArray## xs i,
-          (# p'@(tok', t') #) <- indexSmallArray## ys j =
-            case compare tok tok' of
-              LT -> writeSmallArray zs k p >> go (i + 1) j (k + 1)
-              EQ -> writeSmallArray zs k (tok, t <> t') >> go (i + 1) (j + 1) (k + 1)
-              GT -> writeSmallArray zs k p' >> go i (j + 1) (k + 1)
-
-  go 0 0 0
-  pure zs
-{-# INLINEABLE mergeSmallArray #-}
 
 ifoldrSmallArray :: (Int -> a -> b -> b) -> b -> SmallArray a -> b
 ifoldrSmallArray f z arr = do
